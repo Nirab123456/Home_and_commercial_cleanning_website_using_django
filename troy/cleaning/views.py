bc6872a43fe3489d3553a19523cc08aba7e7fd95
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-09-05 13:29:39 +0600

    starting a new cleaning service website

---
+++ troy/cleaning/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+import datetime
+import calendar
+from calendar import HTMLCalendar
+import time
+from datetime import datetime, timedelta
+from django.shortcuts import get_object_or_404
+from PIL import Image
+import os
+from django.http import FileResponse,HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from datetime import timedelta
+
+
+def home(request):
+    return render(request,'home.html')
