3fd61bfbec295336cf02831d98b9d42bc19d3fea
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-09-08 22:24:55 +0600

    cleaning milstone 11

--- troy/cleaning/admin.py
+++ troy/cleaning/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import contact_to_hire
 
 # Register your models here.
+
+@admin.register(contact_to_hire)
+class contact_to_hireAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone','subject','message','company','date')
+    list_filter = ('name', 'email', 'phone','subject','message','company','date')
+    ordering = ('name',)
+    search_fields = ('name', 'email', 'phone','subject','message','company','date')
+    
--- troy/cleaning/models.py
+++ troy/cleaning/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+
+
+class contact_to_hire(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(max_length=1000,blank=True,null=True)
+    company = models.CharField(max_length=100,blank=True,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+    
+
+    
