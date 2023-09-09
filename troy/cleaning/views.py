f303381654cdeeb7d43c97d39885a3ad52dada86
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-09-10 05:22:45 +0600

    cleaning milstone 22

--- troy/cleaning/views.py
+++ troy/cleaning/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-import datetime
-import calendar
-from calendar import HTMLCalendar
-import time
 from datetime import datetime, timedelta
 from django.shortcuts import get_object_or_404
-from PIL import Image
-import os
 from django.http import FileResponse,HttpResponse
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from datetime import timedelta
 from .models import contact_to_hire, Record_mail_me , SUBSCRIPTION
 from .models import appointment as APPOINMENT
 
