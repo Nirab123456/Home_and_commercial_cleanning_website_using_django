13d57e08f1c295947f16a7083767494e8ca50e5a
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-09-10 18:53:29 +0600

    cleaning staticfiles fixed domain name

--- troy/troy/urls.py
+++ troy/troy/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cleaning.urls')),
     
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     
