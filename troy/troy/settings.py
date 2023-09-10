b84564f4163635f0df52963ef1a69abf2e2ee0a9
nirab123456 <2021-3-70-096@std.ewubd.edu>
2023-09-10 06:06:02 +0600

    cleaning milstone 21

--- troy/troy/settings.py
+++ troy/troy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-rgjndr&73spi-+bsb2rr+lf9ljpe5lf+8^_44-747*_wxvwx$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['troy-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','troy-production-b741.up.railway.app']
 
 
 # Application definition
